cf1d25042340e0023bf2b3a4518fb4ed384bb06c
Reza <Rezasaadatim121@gmail.com>
2019-06-13 20:41:31 +0430

    Initial commit

---
+++ app/src/main/java/gugu/com/guguitem/guguActivity.java
@@ -0,0 +1,13 @@
+package gugu.com.guguitem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class guguActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gugu_item);
+    }
+}
